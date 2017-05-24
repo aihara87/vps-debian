de7afc140b03d50130652c646fcd6c85d5f47f02
aihara87 <rahardiansah1@gmail.com>
2017-05-24 09:07:31 +0700

    Create usernew.sh

---
+++ usernew.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+read -p "Username : " Login
+read -p "Password : " Pass
+read -p "Expired (hari): " mumetndase
+IP=`dig +short myip.opendns.com @resolver1.opendns.com`
+useradd -e `date -d "$mumetndase days" +"%Y-%m-%d"` -s /bin/false -M $Login
+exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+echo -e ""
+echo -e "Informasi SSH"
+echo -e "=========-account-=========="
+echo -e "IP Host  : $IP "
+echo -e "Port     : 443 (Dropbear)"
+echo -e "Username : $Login"
+echo -e "Password : $Pass"
+echo -e "----------------------------"
+echo -e "-->Aktif Sampai: $exp "<--
+echo -e "==========================="
